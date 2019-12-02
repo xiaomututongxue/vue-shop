856f21258ecc00434eae88226e912fede9efb11b
xiaodu <306968428@qq.com>
2019-12-02 12:47:06 +0800

    这是我第一次提交

---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<div id="login">
+			
+		</div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+	#login {
+		width: 400px;
+		height: 300px;
+		background-color: #fff;
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%,-50%);
+		border-radius: 15px;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+//引入elementUI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' 
+import './assets/rest.css'
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
